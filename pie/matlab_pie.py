5ebd8437611ca821d21cd0cda90abf88fc2d3180
NickSheva <nikolayshevtsov@mail.ru>
2023-12-25 14:44:21 +0500

    use tight_layout() to improve details our project, set bbox_to_anchor and ncol

--- pie/matlab_pie.py
+++ pie/matlab_pie.py
@@ -12,6 +12,7 @@ fig, ax = plt.subplots()
 ax.pie(x=size, labels=None, colors=colors, startangle=-55, autopct='%.f%%',
        textprops=text_props)
 
-ax.legend(labels, loc='best')
+ax.legend(labels, bbox_to_anchor=(1.1, 1.15), ncol=3)
+plt.tight_layout()
 # отобржаем на экране
 plt.show()

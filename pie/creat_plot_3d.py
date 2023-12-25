3cdc642c44be783e765f88e147479af40b422067
NickSheva <nikolayshevtsov@mail.ru>
2023-12-25 16:13:34 +0500

    init 3d

---
+++ pie/creat_plot_3d.py
@@ -0,0 +1,12 @@
+"""Importing necessary packages"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Creation an empty figure
+fig = plt.figure()
+
+# Defining the exes as 3D
+ax = plt.axes(projection='3d')
+
+# Displaying the plot
+plt.show()

c10e692a882f9e0010f2fb93403a6b38f2468192
NickSheva <nikolayshevtsov@mail.ru>
2023-12-25 16:21:03 +0500

    add module mplot3d from mpl_toolkits and creat 3d plot

--- pie/creat_plot_3d.py
+++ pie/creat_plot_3d.py
@@ -1,4 +1,5 @@
 """Importing necessary packages"""
+from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,5 +9,12 @@ fig = plt.figure()
 # Defining the exes as 3D
 ax = plt.axes(projection='3d')
 
+# Defining all three axes
+z = np.linspace(0, 1, 100)
+y = z * np.cos(25 * z)
+x = z * np.sin(25 * z)
+# Plotting
+ax.plot(x, y, z, c='red')
+ax.set_title("A simple 3d line plot")
 # Displaying the plot
 plt.show()

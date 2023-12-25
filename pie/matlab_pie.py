9265bc996dfbf76f07a02383aa3b9feca82d79c5
NickSheva <nikolayshevtsov@mail.ru>
2023-12-25 15:07:43 +0500

    save result with savefig()

--- pie/matlab_pie.py
+++ pie/matlab_pie.py
@@ -9,6 +9,11 @@ colors = ['dodgerblue', 'dimgrey', 'mediumorchid', 'red', 'gold', 'limegreen']
 text_props = {'color': 'white', 'fontsize':10}
 # Создаем рамку и холст
 fig, ax = plt.subplots()
+# Добавляем заголовок
+plt.title(label="Copy table",
+          pad=50,
+          fontdict={'fontsize': 14,
+                    'color': 'green'})
 # создаем рисунок
 ax.pie(x=size, labels=None, colors=colors, startangle=-55, autopct='%.f%%',
        textprops=text_props)
@@ -21,8 +26,8 @@ legend_elements = [Line2D([0], [0], marker='o', markersize=10, lw=0, color='dodg
                    Line2D([0], [0], marker='o', markersize=10, lw=0, color='gold', label='Мероприятия, театры, рестораны'),
                    Line2D([0], [0], marker='o', markersize=10, lw=0, color='dimgrey', label='Хобби')]
 
-ax.legend(handles=legend_elements, bbox_to_anchor=(1.1, 1.16), ncol=3, frameon=False)
+ax.legend(handles=legend_elements, bbox_to_anchor=(1.3, 1.16), ncol=3, frameon=False)
 
 plt.tight_layout()
-# отобржаем на экране
-plt.show()
+# сохраниние графика
+plt.savefig('copy.png')

7cbb11111c5ed124478b93c1449878b59283f549
NickSheva <nikolayshevtsov@mail.ru>
2023-12-25 22:17:47 +0500

    putting on the diagrama and remove path

--- pie/weather.py
+++ pie/weather.py
@@ -1,25 +1,26 @@
 """Work with csv file"""
 import csv
-from pathlib import Path
+import matplotlib.pyplot as plt
 
+filename = 'weather/sitka_weather_07-2018_simple.csv'
 
-filename = Path('weather/sitka_weather_07-2018_simple.csv')
-# reading CSV file
+with open(filename, 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    print(header)
+    print('\n'.join(f"{i} {value}" for i, value in enumerate(header)))
+    highs, lows = [], []
+    for row in reader:
+        high = int(row[5])
+        highs.append(high)
+        low = int(row[6])
+        lows.append(low)
+    print(highs)
+    print(lows)
+    # Нанесение данных на диаграмму.
+    plt.style.use('seaborn-v0_8-paper')
+    fig, ax = plt.subplots()
+    ax.plot(highs, c='r')
+    ax.plot(lows, c='b')
+    plt.show()
 
-if filename.is_file() and filename.suffix == '.csv':
-    with open(filename, 'r', encoding='utf-8') as f:
-        reader = csv.reader(f)
-        header = next(reader)
-        print(header)
-        print('\n'.join(f"{i} {value}" for i, value in enumerate(header)))
-        highs, lows = [], []
-        for row in reader:
-            high = int(row[5])
-            highs.append(high)
-            low = int(row[6])
-            lows.append(low)
-        print(highs)
-        print(lows)
-
-else:
-    print("No")

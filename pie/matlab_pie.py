2bf62960352f7ad2a8c38d5ce2141a927f455e9e
NickSheva <nikolayshevtsov@mail.ru>
2023-12-25 14:29:05 +0500

    let's use English first. Create textprops dict for 'size' and change color, hide label, round it with startangle

--- pie/matlab_pie.py
+++ pie/matlab_pie.py
@@ -5,9 +5,11 @@ size = [40, 5, 5, 10, 10, 30]
 labels = ["Работа", "Время с семьей", "Мероприятия, театры, рестораны",
           "Спорт", "Поездки", "Хобби"]
 colors = ['dodgerblue', 'dimgrey', 'mediumorchid', 'red', 'gold', 'limegreen']
+text_props = {'color': 'white', 'fontsize':10}
 # Создаем рамку и холст
 fig, ax = plt.subplots()
 # создаем рисунок
-ax.pie(x=size, labels=labels, colors=colors)
+ax.pie(x=size, labels=None, colors=colors, startangle=-55, autopct='%.f%%',
+       textprops=text_props)
 # отобржаем на экране
 plt.show()

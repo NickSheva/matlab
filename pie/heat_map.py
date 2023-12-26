12498039dac70dde75e2635531b6ffd115403c93
NickSheva <nikolayshevtsov@mail.ru>
2023-12-26 13:21:15 +0500

    add cmap and interpolation

--- pie/heat_map.py
+++ pie/heat_map.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 # Установка случайныъ данных
 data = np.random.random((12, 12))
-# отображения данных
-plt.imshow(data)
+# Изменение цветовой карты
+plt.imshow(data, cmap='autumn', interpolation='nearest')
 # Установка заголовка
 plt.title("Heat Map 2D")
 plt.show()
